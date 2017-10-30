fb7f22ce7879566a01b523424fd61e6f9603bcf8
‘siwanghu <siwanghu@hotmail.com>
2017-10-30 21:36:48 +0800

    siwanghu

---
+++ src/com/siwanghu/regextoNFA/Node.java
@@ -0,0 +1,24 @@
+package com.siwanghu.regextoNFA;
+
+public class Node {
+    private int id;
+    private static int ID=0;
+     
+    public Node(){
+        this.id=ID++;
+    }
+ 
+    public int getId() {
+        return id;
+    }
+     
+    public static void reset(){
+        ID=0;
+    }
+ 
+    @Override
+    public String toString() {
+        return id+"";
+    }
+     
+}
